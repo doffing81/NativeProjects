5b90f58f036b340a5cb1f2058b1ec335a6ad90ad
Brett Doffing <doffing.brett@gmail.com>
2023-06-20 18:31:15 -0500

    Implement FlatList

--- CurrencyConverter/src/App.tsx
+++ CurrencyConverter/src/App.tsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react';
 import {
+  FlatList,
+  Pressable,
   SafeAreaView,
   ScrollView,
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   useColorScheme,
   View,
 } from 'react-native';
@@ -43,14 +46,46 @@ function App(): JSX.Element {
   }
 
   return (
-    <SafeAreaView>
-      <StatusBar
-        
-      />
-      <View>
-        <Text>1</Text>
+    <>
+      <StatusBar />
+      <View style={styles.container}>
+        <View style={styles.topContainer}>
+          <View style={styles.rupeesContainer}>
+          <Text style={styles.rupee}>₹</Text>
+          <TextInput 
+            maxLength={14}
+            value={inputValue}
+            clearButtonMode='always' // only for iOS
+            onChangeText={setInputValue}
+            keyboardType='number-pad'
+            placeholder='Enter amount in Rupees'
+          />
+          </View>
+          {resultValue && (
+          <Text style={styles.resultTxt}>
+            {resultValue}
+          </Text>
+        )}
+        </View>
+        <View style={styles.bottomContainer}>
+            <FlatList 
+            numColumns={1}
+            data={currencyByRupee}
+            keyExtractor={item => item.name}
+            renderItem={({item}) => (
+              <Pressable style={[
+                styles.button, 
+                targetCurrency === item.name && styles.selected
+              ]}
+              onPress={() => buttonPressed(item)}
+              >
+                <CurrencyButton {...item} />
+              </Pressable>
+            )}
+            />
+        </View>
       </View>
-    </SafeAreaView>
+    </>
   );
 }
 
