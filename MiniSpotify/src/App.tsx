3e20fa334c60af365f7899b71b700ec43f2969cf
Brett Doffing <doffing.brett@gmail.com>
2023-06-29 15:19:04 -0500

    Move `App.tsx`

---
+++ MiniSpotify/src/App.tsx
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+function App(): JSX.Element {
+
+  return (
+    <SafeAreaView>
+      <StatusBar/>
+      <Text>Testing</Text>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
