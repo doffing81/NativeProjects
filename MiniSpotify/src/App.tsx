fb5c2462ca722cea9388a623721b0d13c6a6412c
Brett Doffing <doffing.brett@gmail.com>
2023-07-02 13:24:36 -0500

    Conditional setup on load

--- MiniSpotify/src/App.tsx
+++ MiniSpotify/src/App.tsx
@@ -1,22 +1,40 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
+import React , { useState, useEffect } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  ActivityIndicator,
   SafeAreaView,
   StatusBar,
   StyleSheet,
   Text,
   View,
 } from 'react-native';
+import { setupPlayer, addTrack } from "../PlayerService.js"
 
 function App(): JSX.Element {
+  const [isPlayerReady, setIsPlayerReady] = useState(false);
 
+  async function setup() {
+    let isSetup = await setupPlayer()
+
+    if (isSetup) {
+      await addTrack()
+    }
+
+    setIsPlayerReady(isSetup)
+  }
+  
+  useEffect(() => {
+    setup()
+  }, [])
+
+  if (!isPlayerReady) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    )
+  }
+  
   return (
     <SafeAreaView>
       <StatusBar/>
