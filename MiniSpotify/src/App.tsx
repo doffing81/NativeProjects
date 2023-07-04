592ee2a9045603a089ce4bb11399d362b97b519b
Brett Doffing <doffing.brett@gmail.com>
2023-07-04 16:43:22 -0500

    Create SongInfo

--- MiniSpotify/src/App.tsx
+++ MiniSpotify/src/App.tsx
@@ -1,5 +1,5 @@
 import React , { useState, useEffect } from 'react';
-import type {PropsWithChildren} from 'react';
+import type { PropsWithChildren } from 'react';
 import {
   ActivityIndicator,
   SafeAreaView,
